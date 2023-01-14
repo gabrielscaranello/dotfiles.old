8c1722eee8aedc71d23cc35429a432c39a9226b1
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2023-01-14 18:29:46 -0300

    update zsh setup script

--- setup/zsh.sh
+++ setup/zsh.sh
@@ -1,16 +1,64 @@
 #! /bin/sh
 
-Clone Oh My Zsh plugins and theme
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+# Auxiliar DIRS
+PWD=$(pwd)
+ZSH_DIR="zsh"
+BKP_DIR="$HOME/profile-bkp"
+OMZ_DIR="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"
+PLUGINS_DIR="$OMZ_DIR/plugins"
+THEMES_DIR="$OMZ_DIR/themes"
 
-# Remove old config files
-[[ ! -f ~/.p10k.zsh ]] || rm -rf ~/.p10k.zsh
-[[ ! -f ~/.profile ]] || rm -rf ~/.profile
-[[ ! -f ~/.zshrc ]] || rm -rf ~/.zshrc
+# Profile files
+FILES=(.p10k.zsh .profile .zshrc)
 
-cp ./zsh/.p10k.zsh ~/.p10k.zsh
-cp ./zsh/.profile ~/.profile
-cp ./zsh/.zshrc ~/.zshrc
+# Plugins 
+PLUGINS=(zsh-users/zsh-autosuggestions)
 
-source ~/.zshrc
+# Themes
+THEMES=(romkatv/powerlevel10k) 
+
+clone() {
+  repo=$1
+  path=$2
+
+  if [[ ! -d "$path" ]]; then
+    git clone "https://github.com/$repo" "$path"
+  else
+    echo "$repo are installed, skipping..."
+  fi
+}
+
+install_plugins_and_themes() {
+  for plugin in ${PLUGINS[@]}; do clone "$plugin" "${PLUGINS_DIR}/$(echo "$plugin" | cut -d'/' -f2)"; done
+  for theme in ${THEMES[@]}; do clone "$theme" "${THEMES_DIR}/$(echo "$theme" | cut -d'/' -f2)"; done
+}
+
+backup_and_copy_files() {
+  # make backup dir
+  mkdir -p "$BKP_DIR"
+
+  # replace files
+  cd "$ZSH_DIR"
+
+  for file in ${FILES[@]}; do
+    # backup file
+    [[ ! -f "$HOME/$file" ]] || mv "$HOME/$file" "$BKP_DIR/$file"
+    
+    # copy a new file
+    cp "$file" "$HOME/$file"
+  done
+
+  cd "$PWD"
+}
+
+main() {
+  echo $'\nProfile setup started\n'
+  echo "Cloning plugins and themes..."
+  install_plugins_and_themes
+  echo $'\nCnsoping files...\n'
+  backup_and_copy_files
+  echo "Install completed. Old profile files is on $BKP_DIR."
+}
+
+main
+
