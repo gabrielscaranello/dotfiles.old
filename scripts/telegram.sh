6a321548be1e69427322b9646e34722358d7f4d2
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2023-10-02 21:32:50 -0300

    update telegram script

--- scripts/telegram.sh
+++ scripts/telegram.sh
@@ -5,7 +5,8 @@ file_name="tsetup.$version.tar.xz"
 output_dir="/tmp/$file_name"
 download_url="https://github.com/telegramdesktop/tdesktop/releases/download/v$version/$file_name"
 extract_dir="/tmp/Telegram"
-target_dir="$HOME/.local/opt/telegram-desktop"
+opt_dir="$HOME/.local/opt"
+target_dir="$opt_dir/telegram-desktop"
 
 # Remove old file
 rm -rf $output_dir $target_dir $extract_dir
@@ -14,6 +15,7 @@ rm -rf $output_dir $target_dir $extract_dir
 wget -c $download_url -O $output_dir
 
 # Extract file
+mkdir -p $opt_dir
 tar -xf $output_dir -C /tmp
 mv $extract_dir $target_dir
 
