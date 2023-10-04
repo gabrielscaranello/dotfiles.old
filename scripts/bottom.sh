eed2cce1f06c610a2f0c11675c8b13a866bf0528
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2023-10-04 20:54:59 -0300

    fix typo error

--- scripts/bottom.sh
+++ scripts/bottom.sh
@@ -2,16 +2,16 @@
 
 version=$(curl -s "https://api.github.com/repos/ClementTsang/bottom/releases/latest" | grep -Po '"tag_name": "\K[^"]*')
 file_name="bottom_${version}_amd64.deb"
-output_fiile="/tmp/$file_name"
+output_file="/tmp/$file_name"
 download_url="https://github.com/ClementTsang/bottom/releases/download/$version/$file_name"
 
 # Remove old file
-rm -rf $output_fiile
+rm -rf $output_file
 
 # Download file
-wget -c $download_url -O $output_fiile
+wget -c $download_url -O $output_file
 
 # Install
-sudo dpkg -i $output_fiile
+sudo dpkg -i $output_file
 
 echo "Bottom installed..."
