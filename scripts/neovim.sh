4ab491b9db4b9373deab62825cc1a8ba20b87f72
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2024-04-04 20:01:57 -0300

    make neovim build and compatibility better

--- scripts/neovim.sh
+++ scripts/neovim.sh
@@ -9,11 +9,11 @@ WORK_DIR="/tmp/neovim"
 rm -rf $WORK_DIR
 
 # Clone repository
-git clone -b "$BRANCH" "$GIT_URL" "$WORK_DIR"
+git clone --depth 1 -b "$BRANCH" "$GIT_URL" "$WORK_DIR"
 
 # Install
 cd "$WORK_DIR"
-make
+make CMAKE_BUILD_TYPE=RelWithDebInfo
 cd build && cpack -G DEB && sudo dpkg -i nvim-linux64.deb && cd "$WORK_DIR"
 
 echo "Neovim installed..."
